eee181c98f7ffa87174f059454b94dbaa3c20900
Haider Alshamma <haideralsh@gmail.com>
2025-08-31 14:04:35 -0400

    Select parent once all the descendants are selected

--- src-tauri/src/commands/tree/select.rs
+++ src-tauri/src/commands/tree/select.rs
@@ -1,7 +1,44 @@
 use crate::commands::tree::cache::cache;
 use crate::commands::tree::index::ensure_index;
 use crate::errors::DirectoryError;
+use crate::models::TreeIndex;
 use std::collections::HashSet;
+
+fn all_descendants_selected(id: &str, tree_index: &TreeIndex, selected: &HashSet<String>) -> bool {
+    let Some(node) = tree_index.nodes.get(id) else {
+        return false;
+    };
+
+    if node.node_type == "file" {
+        return selected.contains(id);
+    }
+
+    for child in &node.children {
+        if !all_descendants_selected(child, tree_index, selected) {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn update_ancestors_selection(id: &str, tree_index: &TreeIndex, selected: &mut HashSet<String>) {
+    let mut cur = Some(id.to_string());
+    while let Some(cid) = cur {
+        if let Some(node) = tree_index.nodes.get(&cid) {
+            if let Some(parent) = &node.parent {
+                if all_descendants_selected(parent, tree_index, selected) {
+                    selected.insert(parent.clone());
+                } else {
+                    selected.remove(parent);
+                }
+                cur = Some(parent.clone());
+                continue;
+            }
+        }
+        break;
+    }
+}
 
 #[tauri::command]
 pub(crate) fn toggle_selection(
@@ -41,6 +78,8 @@ pub(crate) fn toggle_selection(
             set.remove(&t);
         }
     }
+
+    update_ancestors_selection(&id, idx, &mut set);
 
     Ok(set.into_iter().collect())
 }
