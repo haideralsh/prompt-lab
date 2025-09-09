22d305abdfb00d017d01397d0bee2e3d327c51b2
Haider Alshamma <haideralsh@gmail.com>
2025-09-09 12:36:05 -0400

    Use Inter as a UI font

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './main.css'
 import App from './App'
 import { SidebarContextProvider } from './components/Sidebar/SidebarContext'
+import { Toaster } from './components/ToastQueue'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <SidebarContextProvider>
+      <Toaster />
       <App />
     </SidebarContextProvider>
   </React.StrictMode>,
