b4b4c97e24b9fc80f3d0478c0fee7f04e253d298
Haider Alshamma <haideralsh@gmail.com>
2025-09-11 13:00:47 -0400

    Support deselect in from outside the tree

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,17 @@
 import { useMemo } from 'react'
 import { useSidebarContext } from './Sidebar/SidebarContext'
+import { invoke } from '@tauri-apps/api/core'
+import { SelectionResult } from '../types/FileTree'
 
 export function Main() {
-  const { selectedFiles } = useSidebarContext()
+  const {
+    selectedFiles,
+    directory,
+    selectedNodes,
+    setSelectedFiles,
+    setSelectedNodes,
+    setIndeterminateNodes,
+  } = useSidebarContext()
 
   const sortedFiles = useMemo(() => {
     return selectedFiles.sort((a, b) => {
@@ -13,26 +22,47 @@ export function Main() {
     })
   }, [selectedFiles])
 
+  async function deselect(id: string | number) {
+    const selection = await invoke<SelectionResult>('toggle_selection', {
+      path: directory?.path,
+      current: Array.from(selectedNodes),
+      id: id,
+    })
+    setSelectedNodes(new Set(selection.selectedNodes))
+    setSelectedFiles(selection.selectedFiles)
+    setIndeterminateNodes(new Set(selection.indeterminate))
+  }
+
   return (
-    <section className="flex-1 p-4">
+    <section className="flex-1 p-4 bg-background-dark">
       <h2 className="text-sm font-semibold text-white mb-2">Selected files</h2>
       {sortedFiles.length > 0 && (
         <ul className="space-y-4 text-sm text-white">
           {Array.from(sortedFiles).map((path) => (
             <li key={path.id} className="flex flex-col gap-2">
-              <span className="flex items-center gap-1">
-                <span className="font-semibold">{path.title}</span>
-                <span className="text-gray-400">
-                  {path.tokenCount == null
-                    ? 'counting...'
-                    : `${path.tokenCount} tokens${
-                        path.tokenPercentage == null
-                          ? ''
-                          : ` (${Math.ceil(path.tokenPercentage)}%)`
-                      }`}
-                </span>
-              </span>
-              <span className="text-xs">{path.id}</span>
+              <div className="flex justify-between items-center">
+                <div className="flex flex-col gap-2">
+                  <span className="flex items-center gap-1">
+                    <span className="font-semibold">{path.title}</span>
+                    <span className="text-gray-400">
+                      {path.tokenCount == null
+                        ? 'counting...'
+                        : `${path.tokenCount} tokens${
+                            path.tokenPercentage == null
+                              ? ''
+                              : ` (${Math.ceil(path.tokenPercentage)}%)`
+                          }`}
+                    </span>
+                  </span>
+                  <span className="text-xs">{path.id}</span>
+                </div>
+                <button
+                  className="text-xs bg-interactive-dark hover:bg-interactive-mid active:bg-interactive-light flex items-center col gap-1.5 rounded-xs cursor-pointer px-2 py-1 w-fit text-text-light"
+                  onClick={() => deselect(path.id)}
+                >
+                  Deselect
+                </button>
+              </div>
             </li>
           ))}
         </ul>
