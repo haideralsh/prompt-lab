6094692830f3ab56534fa022cf83ad6849c8dfcf
Haider Alshamma <haideralsh@gmail.com>
2025-09-26 12:50:21 -0400

    Redesign selected files UI for improved clarity and usability

--- src/components/layout/SelectedFilesPanel.tsx
+++ src/components/layout/SelectedFilesPanel.tsx
@@ -5,6 +5,7 @@ import { CheckIcon } from '@radix-ui/react-icons'
 import { PanelDisclosure } from './PanelDisclosure'
 import { useSidebarContext } from '../Sidebar/SidebarContext'
 import { Id, SelectionResult } from '../../types/FileTree'
+import { CopyButton } from '../common/CopyButton'
 
 export function SelectedFilesPanel() {
   const {
@@ -15,6 +16,7 @@ export function SelectedFilesPanel() {
     setSelectedNodes,
     setIndeterminateNodes,
     totalTokenCount,
+    tree,
   } = useSidebarContext()
 
   const sortedFiles = useMemo(() => {
@@ -37,12 +39,26 @@ export function SelectedFilesPanel() {
     setIndeterminateNodes(new Set(selection.indeterminateNodesPaths))
   }
 
+  async function copyFile(path: Id) {
+    if (!directory?.path) return
+
+    await invoke('copy_files_to_clipboard', {
+      treeMode: 'selected',
+      fullTree: tree,
+      selectedNodes: [path],
+      gitDiffPaths: [],
+      root: directory.path,
+      urls: [],
+    })
+  }
+
   return (
     <PanelDisclosure
       id="selected"
       label="Selected files"
       count={sortedFiles.length}
       headingClassName="flex items-center gap-1 text-text-dark"
+      panelClassName="p-2 flex flex-col gap-1"
       iconClassName=""
       isGroupSelected={false}
       isGroupIndeterminate={false}
@@ -58,7 +74,7 @@ export function SelectedFilesPanel() {
               <Checkbox
                 defaultSelected
                 slot="selection"
-                className="grid grid-cols-[auto_1fr] items-start gap-x-3 gap-y-1 group text-left w-full rounded-sm px-2 py-1 hover:bg-accent-interactive-dark data-[hovered]:bg-accent-interactive-dark"
+                className="grid grid-cols-[auto_1fr_auto] items-center gap-x-3 gap-y-1 group text-left w-full rounded-sm px-2 py-0.5 hover:bg-accent-interactive-dark data-[hovered]:bg-accent-interactive-dark"
                 onChange={(isSelected) => {
                   if (!isSelected) void deselect(file.path)
                 }}
@@ -66,30 +82,34 @@ export function SelectedFilesPanel() {
                 {({ isSelected }) => (
                   <>
                     <span
-                      className="flex items-center justify-center mt-0.5 size-[15px] rounded-sm text-accent-text-light
+                      className="flex items-center justify-center size-[15px] rounded-sm text-accent-text-light
                                   border border-border-light  group-data-[selected]:border-accent-border-mid group-data-[indeterminate]:border-accent-border-mid
                                   bg-transparent group-data-[selected]:bg-accent-interactive-light group-data-[indeterminate]:bg-accent-interactive-light
                                   flex-shrink-0"
                     >
                       {isSelected && <CheckIcon />}
                     </span>
-                    <span className="flex flex-col text-left">
-                      <span className="flex items-baseline gap-2">
-                        <span className="font-normal text-text-dark break-all">
-                          {file.title}
-                        </span>
-                        <span className="text-xs text-solid-light">
+                    <span className="flex items-center gap-1.5 w-full min-w-0">
+                      <span className="font-normal text-text-dark break-all truncate">
+                        {file.title}
+                      </span>
+                      <span className="hidden group-hover:inline text-solid-light truncate">
+                        {file.prettyPath}
+                      </span>
+                    </span>
+                    <span>
+                      <span className="hidden group-hover:flex group-hover:items-center group-hover:gap-1.5">
+                        <CopyButton
+                          onCopy={async () => {
+                            await copyFile(file.path)
+                          }}
+                          className="text-text-light/75 hover:text-text-light data-[disabled]:text-text-light/75"
+                        />
+                        <span className="text-solid-light text-xs border border-border-dark px-1 rounded-sm group-hover:text-text-dark group-hover:border-border-light">
                           {file.tokenCount == null
                             ? 'counting...'
-                            : `${file.tokenCount} tokens${
-                                file.tokenPercentage == null
-                                  ? ''
-                                  : ` (${Math.ceil(file.tokenPercentage)}%)`
-                              }`}
+                            : file.tokenCount.toLocaleString()}
                         </span>
-                      </span>
-                      <span className="text-xs text-solid-light break-all">
-                        {file.prettyPath}
                       </span>
                     </span>
                   </>
@@ -99,7 +119,7 @@ export function SelectedFilesPanel() {
           ))}
         </ul>
       ) : (
-        <div className="text-xs text-text-dark">
+        <div className="text-xs text-solid-light">
           Files selected in the tree will appear here.
         </div>
       )}
