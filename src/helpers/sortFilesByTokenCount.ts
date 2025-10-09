93e1f505da385f0fc7097f7ec347a6a1b2265ab1
Haider Alshamma <haideralsh@gmail.com>
2025-10-09 15:11:20 -0400

    Use the path instead of the title to make sure we don't have duplicates

--- src/helpers/sortFilesByTokenCount.ts
+++ src/helpers/sortFilesByTokenCount.ts
@@ -3,7 +3,7 @@ import type { FileNode } from '../types/FileTree'
 export function sortFilesByTokenCount(files: FileNode[]) {
   return files.sort((a, b) => {
     if (a.tokenCount == null && b.tokenCount == null) {
-      return a.title.localeCompare(b.title)
+      return a.path.localeCompare(b.path)
     }
     if (a.tokenCount == null) return 1
     if (b.tokenCount == null) return -1
@@ -11,6 +11,6 @@ export function sortFilesByTokenCount(files: FileNode[]) {
     const countDiff = b.tokenCount - a.tokenCount
     if (countDiff !== 0) return countDiff
 
-    return a.title.localeCompare(b.title)
+    return a.path.localeCompare(b.path)
   })
 }
