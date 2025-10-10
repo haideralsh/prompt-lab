99f2df3517dfbc8884a2059dd50959ca0e43546d
Haider Alshamma <haideralsh@gmail.com>
2025-10-10 14:39:54 -0400

    adjust Scrollbar styling

--- src/components/ScrollArea.tsx
+++ src/components/ScrollArea.tsx
@@ -6,7 +6,7 @@ export function ScrollArea({ children }: React.PropsWithChildren) {
       <BaseUiScrollArea.Viewport className="h-full overscroll-contain">
         {children}
       </BaseUiScrollArea.Viewport>
-      <BaseUiScrollArea.Scrollbar className="m-1 flex w-1.5 justify-center rounded opacity-0 transition-opacity delay-300 data-[hovering]:opacity-100 data-[hovering]:delay-0 data-[hovering]:duration-75 data-[scrolling]:opacity-100 data-[scrolling]:delay-0 data-[scrolling]:duration-75">
+      <BaseUiScrollArea.Scrollbar className="m-1 flex w-1 justify-center rounded opacity-0 transition-opacity delay-300 data-[hovering]:opacity-100 data-[hovering]:delay-0 data-[hovering]:duration-75 data-[scrolling]:opacity-100 data-[scrolling]:delay-0 data-[scrolling]:duration-75">
         <BaseUiScrollArea.Thumb className="w-full rounded bg-border-mid" />
       </BaseUiScrollArea.Scrollbar>
     </BaseUiScrollArea.Root>
