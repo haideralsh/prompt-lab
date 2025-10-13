e5cb4b3a6f34360fcaad885902034ecc729f45f2
Haider Alshamma <haideralsh@gmail.com>
2025-10-12 21:15:44 -0400

    Add resizable sidebar handle component

---
+++ src/components/layout/SidebarResizeHandle.tsx
@@ -0,0 +1,25 @@
+interface SidebarResizeHandleProps {
+  startDragging: (e: React.MouseEvent<Element, MouseEvent>) => void
+  isDragging: boolean
+}
+
+export function SidebarResizeHandle({
+  startDragging,
+  isDragging,
+}: SidebarResizeHandleProps) {
+  return (
+    <div
+      onMouseDown={startDragging}
+      data-sidebar-handle
+      className="absolute -right-0.25 top-0 h-full w-2 cursor-col-resize select-none group"
+    >
+      <div
+        className={`absolute right-0 top-0 h-full transition-all z-20 ${
+          isDragging
+            ? 'w-1 bg-accent-border-dark'
+            : 'w-0 bg-accent-border-dark group-hover:w-1'
+        }`}
+      />
+    </div>
+  )
+}
