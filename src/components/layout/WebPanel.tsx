d15d4fdce023d7ee08bd6738097a2eec057ed6e8
Haider Alshamma <haideralsh@gmail.com>
2025-10-13 23:33:27 -0400

    Add text selection color

--- src/components/layout/WebPanel.tsx
+++ src/components/layout/WebPanel.tsx
@@ -401,7 +401,7 @@ export function WebDisclosurePanel() {
                   <Button
                     type="submit"
                     isDisabled={isSavingWeb || !webUrl.trim()}
-                    className="text-xs  tracking-wide p-1 flex items-center justify-center rounded-sm  text-text-dark data-[disabled]:text-text-dark/60  hover:text-text-light"
+                    className="text-xs tracking-wide p-1 flex items-center justify-center rounded-sm  text-text-dark data-[disabled]:text-text-dark/60  hover:text-text-light"
                   >
                     Add
                   </Button>
