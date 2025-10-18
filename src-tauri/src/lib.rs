0a0f79d176104452d3a125b0a349a84909e081a5
Haider Alshamma <haideralsh@gmail.com>
2025-10-17 23:13:37 -0400

    Remove the default webview shortcuts

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_prevent_default::init())
         .invoke_handler(tauri::generate_handler![
             api::directory::command::pick::pick_directory,
             api::directory::command::list::list_directory,
